9631f36acf28f0c1a4b86cb4b075efee079f9720
lblva <lola.batchalova@hotmail.com>
2024-03-04 10:30:33 +0100

    prettier toegevoegd

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './styles/style.css'
-import './styles/reset.css'
+import './styles/style.css';
+import './styles/reset.css';
 // core version + navigation, pagination modules:
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
@@ -7,7 +7,7 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import {apiKey} from "./key.js";
+import { apiKey } from './key.js';
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {
@@ -30,29 +30,29 @@ const swiper = new Swiper('.swiper', {
 });
 
 // fetch weather
-async function getweather(){
-  const cities = document.querySelectorAll(".swiper-slide");
-  
-    cities.forEach(async city => {
-    const cityElement = city.querySelector(".image-text");
+async function getweather() {
+  const cities = document.querySelectorAll('.swiper-slide');
+
+  cities.forEach(async (city) => {
+    const cityElement = city.querySelector('.image-text');
     const lat = cityElement.dataset.lat;
     const lon = cityElement.dataset.lon;
     console.log(lat, lon);
- 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`,
+    );
     const data = await response.json();
 
     const temperature = data.main.temp;
     console.log(temperature);
 
-    const temperatureElement = city.querySelector(".temperature__value");
+    const temperatureElement = city.querySelector('.temperature__value');
     temperatureElement.innerHTML = `&nbsp;${Math.round(temperature)}°C`;
-    const loaderElement = city.querySelector(".temperature__loader")
-    loaderElement.style.display= "none";
-    temperatureElement.style.display = "block";
-
-
-  })
+    const loaderElement = city.querySelector('.temperature__loader');
+    loaderElement.style.display = 'none';
+    temperatureElement.style.display = 'block';
+  });
 }
 
 getweather();
