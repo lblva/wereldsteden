012ed15757d82c9e8643a9e91b337f7a219afd0c
lblva <lola.batchalova@hotmail.com>
2024-03-18 10:46:00 +0100

    three.js wereldkaart base gemaakt

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { apiKey } from './key.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // init Swiper:
 new Swiper('.swiper', {
@@ -56,3 +58,39 @@ async function getweather() {
 }
 
 getweather();
+
+const width = window.innerWidth,
+  height = 500;
+
+// Init scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+camera.position.z = 1;
+
+// Load Earth texture
+const loader = new THREE.TextureLoader();
+const earthTexture = loader.load('wereldkaart.jpg');
+const geometry = new THREE.SphereGeometry(0.5, 32, 32);
+const material = new THREE.MeshBasicMaterial({ map: earthTexture });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Init renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+// Add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.rotateSpeed = 0.35;
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
